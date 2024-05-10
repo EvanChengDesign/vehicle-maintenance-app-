3bb5a867cf2c75c031acf84b011d27601a74268d
Evan Cheng <evanchengdesign@gmail.com>
2024-05-09 21:24:55 -0400

    made all models a collection

--- src/models/index.js
+++ src/models/index.js
@@ -10,20 +10,23 @@ const partsModel = require('./parts/model.js')
 const userModel = require('../auth/models/users.js');
 
 const Collection = require('./data-collection.js');
+// const { Col } = require('sequelize/lib/utils');
 
-const environment = process.env.NODE_ENV;
+// const environment = process.env.NODE_ENV;
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 
 const sequelize = new Sequelize(DATABASE_URL, {logging:false});
 
 const food = foodModel(sequelize, DataTypes);
 const clothes = clothesModel(sequelize, DataTypes);
+const vehicles = vehicleModel(sequelize, DataTypes);
+const parts = partsModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
   users: userModel(sequelize, DataTypes),
-  vehicles: vehicleModel(sequelize, DataTypes),
-  parts: partsModel(sequelize, DataTypes),
+  vehicles: new Collection(vehicles),
+  parts: new Collection(parts),
   food: new Collection(food),
   clothes: new Collection(clothes),
 };
